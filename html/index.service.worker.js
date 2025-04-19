fc62553f7084d3d4ca79c65dfea3762e8498930c
Bryan Lorenzo <bryan@client.lan>
2025-04-19 21:53:34 +0800

    bob_roll no debug

--- html/index.service.worker.js
+++ html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745070653|5074005901';
+const CACHE_VERSION = '1745070796|5217263062';
 /** @type {string} */
 const CACHE_PREFIX = 'water-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
